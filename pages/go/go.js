9fe6de6876d9b9e5ffde78b9b27ce84dad941e26
chenchong <chenchong55@qq.com>
2018-03-15 10:48:43 +0800

    input not finished

--- pages/go/go.js
+++ pages/go/go.js
@@ -1,22 +1,24 @@
 const app = getApp()
-var QQMapWX = require('../../libs/qqmap-wx-jssdk.js')
+var QQMapWX = require('../../libs/qqmap-wx-jssdk.min.js')
 var qqmapsdk
 var timer
+var that
 
 Page({
   formSubmit: function(e) {
-   console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
   },
   data: {
-    imgUrl:[
-      '../../assets/icons/location/where.png'
+    imgUrl: [
+      '../../assets/icons/location/where2.png'
     ],
-    wd:'',
-    jd:'',
-    address:'',
-    addressList:[]
+    wd: '',
+    jd: '',
+    address: '',
+    city: '',
+    addressList: []
   },
-  Location_where(){
+  Location_where() {
     wx.getLocation({
       type: 'gcj02', //返回可以用于wx.openLocation的经纬度
       success: function(res) {
@@ -25,71 +27,64 @@ Page({
         wx.openLocation({
           latitude: latitude,
           longitude: longitude,
-         scale: 28
+          scale: 28
+        })
+      }
     })
-  }
-})
-},
-bindKeyInput:(e) => {
-  console.log(e);
-  var that = this
-  qqmapsdk = new QQMapWX({
-    key: 'NU6BZ-GBTH6-GZLSW-MSY6G-457DS-2JFGV' // 必填
-  }),
-  qqmapsdk.getSuggestion({
-    keyword: e.detail.value,
-    success: (res) =>{
-        let addressList = res.data
-        this.setData({
-            addressList:res.data
-        });
-
-    },
-    fail: function (res) {
-        console.log(res);
-    },
-    complete: function (res) {
-      console.log(res);
-    }
-  })
-},
-
-
-  onLoad () {
+  },
+  bindKeyInput: function(e) {
     var that = this
+    qqmapsdk = new QQMapWX({
+      key: 'NU6BZ-GBTH6-GZLSW-MSY6G-457DS-2JFGV' // 必填
+    })
+    qqmapsdk.getSuggestion({
+      keyword: e.detail.value,
+      region: that.data.city,
+      success: function(res) {
+        console.log(res.data);
+        that.setData({
+          addressList: res.data
+        })
+      }
+    })
+  },
+  onLoad() {
+    that = this;
     // 实例化腾讯地图API核心类
     qqmapsdk = new QQMapWX({
       key: 'NU6BZ-GBTH6-GZLSW-MSY6G-457DS-2JFGV' // 必填
     })
     qqmapsdk.getSuggestion({
 
-    }),
-    wx.getLocation({
-      type: 'wgs84',
+      }),
+      wx.getLocation({
+        type: 'wgs84',
 
-      success:(res) => {
-        console.log(res);
-        var latitude_1= res.latitude
-        var longitude_1= res.longitude
-        qqmapsdk.reverseGeocoder({
-        location: {
-          latitude: latitude_1,
-          longitude: longitude_1
-        },
-        success: function (addressRes) {
-         var address = addressRes.result.formatted_addresses.recommend;
-         console.log(address);
+        success: (res) => {
+          console.log(res);
+          var latitude_1 = res.latitude
+          var longitude_1 = res.longitude
+          qqmapsdk.reverseGeocoder({
+            location: {
+              latitude: latitude_1,
+              longitude: longitude_1
+            },
+            success: function(addressRes) {
+              var address = addressRes.result.formatted_addresses
+                .recommend;
+              console.log(address);
 
-          that.setData({
-            address: address,
-            wd:latitude_1,
-            jd:longitude_1
+              that.setData({
+                address: address,
+                wd: latitude_1,
+                jd: longitude_1,
+                city: addressRes.result.address_component.city
+              })
+            }
           })
         }
       })
-      }
-    })
-    // Countdown()
+      // Countdown()
 
 
 
